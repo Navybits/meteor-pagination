d4c3a9497a7428d1d69561e53b8d0fe6bffbe0d4
taha <taha.ziadeh@navybits.com>
2017-01-03 10:24:23 +0200

    git url added to readme

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Paginate whatever you want, wherever you want, whenever you want and how much ever you want. A meteor pagination package by Navybits',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/Navybits/meteor-pagination.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
